6079f85a96adc645d4821f3cdbe070e758b1bbbe
oxy-Op <mdrehaan7766@gmail.com>
2023-05-25 22:14:16 +0530

    This is when i decided to change js delegate to onclick and try to change ui

--- index.js
+++ index.js
@@ -95,16 +95,7 @@
 //     })
 // });
 
-$('.task_delete').on('click', function () {
-    var me = (this)
-    $(me).parents('li').animate({
-        width: 0,
-        height: 0,
-        opacity: 0
-    }, 400, function () {
-        $(me).parents('li').remove();
-    })
-})
+
 
 $(document).ready(function () {
     $("#task_text").on({
@@ -120,19 +111,68 @@ $(document).ready(function () {
     });
 });
 
-$(document).ready(function () {
-    $(".task_delete").on({
-        input: function () {
+
+
+
+$(document).delegate('.task_delete','click', function () {
+    var me = $(this);
+    $(me).parents('li').animate({
+        width: 0,
+        height: 0,
+        opacity: 0
+    }, 400, function () {
+        $(me).parents('li').remove();
+    })
+})
+
+$(document).delegate(
+    '.task_edit',
+    'click', function () {
+        var span = $(this).parents('li').children('span');
+        var input = $('<textarea id="editarea" autocomplete="off" maxlength=200 class="taskedit" rows=5 >').val(span.text());
+        input.css({
+            height: span.height(),
+            width: span.width()
+        })
+        span.replaceWith(input);
+        $('#editarea').on('input', function () {
             $(this).height(0); // Reset the height to 0
             $(this).height(this.scrollHeight);
-        },
-        keypress: function () {
-            $(this).css({
-                borderBottom: '1.5px solid saddlebrown'
-            }, 500).show(200);
+            submit.prop('disabled', false);
+            if ($(this).val().trim() === "") {
+                submit.prop('disabled', true);
+            }
         }
-    });
-});
+        )
+        var submit = $('<button type="submit" class="tasksubmit">Submit</button>');
+        var cancel = $('<button type="submit" class="cancelsubmit">Cancel</button>');
+        var li = $(this).parents('li');
+        var editoptions = $('<div class="editoptions"></div>').append(submit,cancel);
+        li.append(editoptions);
+        li.css('flex-direction', 'column');
+        li.children('div .options').hide();
+        submit.prop('disabled',true);
+        $(document).delegate(
+            '.cancelsubmit', 'click', function () {
+                li.css('flex-direction', 'row');
+                li.children('div .editoptions').hide();
+                li.children('div .options').show(500);
+                span.show();
+                input.hide();
+            }
+        )
+        $(document).delegate('.tasksubmit', 'click', function (e) {
+            li.css('flex-direction', 'row');
+            li.children('div .editoptions').hide();
+            li.children('div .options').show(500);
+            var s = $('<span>').text(input.val());
+            input.replaceWith(s);
+            input.css({
+                height: 0,
+                width: 0
+            })
+        })
+    })
 
 
 
@@ -149,7 +189,7 @@ function addTask() {
         return false;
     }
     let userInput = textarea_.val();
-
+    console.log(userInput)
     let item = $('<li>').append($('<span>').text(userInput));
     item.hide().appendTo('.tasks').fadeOut(500, function () {
         $(this).fadeIn(500, function () {
@@ -157,27 +197,12 @@ function addTask() {
     });
     // item.
     $(".tasks").append(item);
-    $(".tasks li").last().append("<div><button class = 'task_edit' ><img src='assets/edit.svg' alt='edit svg'></button><button class='task_delete' ><img src='assets/delete.svg' alt='delete svg'></button></div>")
+    $(".tasks li").last().append("<div class='options'><button class = 'task_edit' ><img src='assets/edit.svg' alt='edit svg'></button><button class='task_delete' ><img src='assets/delete.svg' alt='delete svg'></button></div>")
     textarea_.val('');
     textarea_.height('');
     var myElement = $('.tasks');
     myElement.scrollTop((myElement[0].scrollHeight + 100) - myElement.outerHeight());
-    $('.task_delete').on('click', function () {
-        var me = (this)
-        $(me).parents('li').animate({
-            width: 0,
-            // height: 0,
-            opacity: 0
-        },400,function(){
-            $(me).parents('li').remove();
-        })
-    })
-
-    $('.task_edit').on('click',function(){
-        var span = $(this).parents('li').children('span')
-        var input = $('<input type="text" autocomplete="off" maxlength=200 class="taskedit">').val(span.text());
-        span.replaceWith(input)
-    })
+    
 };
 
 function addList() {
@@ -198,7 +223,7 @@ function addList() {
                     myElement.scrollTop((myElement[0].scrollHeight) - myElement.outerHeight())
                 });
                 $('.lists').append(item);
-                $(".lists li").last().append("<div><button class = 'task_edit' ><img src='assets/edit.svg' alt='edit svg'></button><button class='task_delete'><img src='assets/delete.svg' alt='delete svg'></button></div>")
+                $(".lists li").last().append("<div><button class = 'list_task_edit' ><img src='assets/edit.svg' alt='edit svg'></button><button class='task_delete'><img src='assets/delete.svg' alt='delete svg'></button></div>")
                 
                 parent.animate({
                     opacity: 0
@@ -209,21 +234,7 @@ function addList() {
                 }, 200, function () {
                     parent.children().replaceWith(child)
                 })
-                
-                $('.task_delete').on('click', function () {
-                    var me = (this)
-                    $(me).parents('li').animate({
-                        width: 0,
-                        height: 0,
-                        opacity: 0
-                    }, 400, function () {
-                        $(me).parents('li').remove();
-                    })
-                })
-                
             }
-
-
         },
         blur: function () {
             $(this).animate({
@@ -236,7 +247,6 @@ function addList() {
                     }, 400, function () {
                         parent.children().replaceWith(child)
                     })
-
                 });
         },
     }
